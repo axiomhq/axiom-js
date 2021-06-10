10828eafb8418ef5d34b53256d1c793190bd760f
Arne Bahlo <arne@axiom.co>
2021-06-10 12:20:27 +0200

    Create and delete dataset for integration tests

--- tests/integration/datasets.test.ts
+++ tests/integration/datasets.test.ts
@@ -4,9 +4,22 @@ import DatasetsService from '../../lib/datasets';
 
 const deploymentURL = process.env.AXM_DEPLOYMENT_URL!;
 const accessToken = process.env.AXM_ACCESS_TOKEN!;
+const datasetSuffix = process.env.DATASET_SUFFIX || 'local';
 
 describe('DatasetsService', () => {
+    let datasetName = `test-integration-axiom-node-${datasetSuffix}`;
     const client = new DatasetsService(deploymentURL, accessToken);
+
+    before(async () => {
+        await client.create({
+            name: datasetName,
+            description: 'Automatically created by axiom-node integration tests.',
+        });
+    });
+
+    after(async () => {
+        await client.delete(datasetName);
+    });
 
     describe('Stats', () => {
         it('returns a valid response', async () => {
