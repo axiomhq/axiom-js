67409938cba0893cce763f76d4a00551fba9df5a
Lila Kurse <lila@axiom.co>
2021-11-02 16:02:11 +0000

    Modified sample to use namespace

--- examples/ingest-file.ts
+++ examples/ingest-file.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import Client, { CloudURL } from '../lib';
-import { ContentEncoding, ContentType } from '../lib/datasets';
+import { datasets } from '../lib/datasets';
 
 const deploymentURL = process.env.AXIOM_URL || CloudURL;
 const accessToken = process.env.AXIOM_TOKEN;
@@ -8,7 +8,12 @@ const client = new Client(deploymentURL, accessToken);
 
 async function ingestFile() {
     const stream = fs.createReadStream('logs.json');
-    const res = await client.datasets.ingest('test', stream, ContentType.JSON, ContentEncoding.Identity);
+    const res = await client.datasets.ingest(
+        'test',
+        stream,
+        datasets.ContentType.JSON,
+        datasets.ContentEncoding.Identity,
+    );
     console.log('Ingested %d events with %d failures', res.ingested, res.failed);
     // Ingested 3 events with 0 failures
 }
