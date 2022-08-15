d4c45ed497644cfa631150ab3214619e9f1579a9
Islam Shehata <islam@axiom.co>
2022-08-15 13:54:56 +0200

    add example for ingestEvents

---
+++ examples/ingest-events.ts
@@ -0,0 +1,18 @@
+import Client, { datasets } from '@axiomhq/axiom-node';
+
+const client = new Client();
+
+async function ingest() {
+    const events = [{
+        'foo': 'bar'
+    },
+    {
+        'x': 'y'
+    }];
+
+    const res = await client.datasets.ingestEvents('test', events);
+    console.log('Ingested %d events with %d failures', res.ingested, res.failed);
+    // Ingested 2 events with 0 failures
+}
+
+ingest();
