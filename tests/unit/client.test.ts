36d0f08ef7332f6048c9fed839e9d0c28078ef1a
Islam Shehata <islam@axiom.co>
2022-11-25 18:47:40 +0200

    move nock scope from datasets test to client test

--- tests/unit/client.test.ts
+++ tests/unit/client.test.ts
@@ -17,6 +17,65 @@ import {
     headerRateScope,
 } from '../../lib/limit';
 
+const queryLegacyResult = {
+    status: {
+        elapsedTime: 542114,
+        blocksExamined: 4,
+        rowsExamined: 142655,
+        rowsMatched: 142655,
+        numGroups: 0,
+        isPartial: false,
+        cacheStatus: 1,
+        minBlockTime: '2020-11-19T11:06:31.569475746Z',
+        maxBlockTime: '2020-11-27T12:06:38.966791794Z',
+    },
+    matches: [
+        {
+            _time: '2020-11-19T11:06:31.569475746Z',
+            _sysTime: '2020-11-19T11:06:31.581384524Z',
+            _rowId: 'c776x1uafkpu-4918f6cb9000095-0',
+            data: {
+                agent: 'Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)',
+                bytes: 0,
+                referrer: '-',
+                remote_ip: '93.180.71.3',
+                remote_user: '-',
+                request: 'GET /downloads/product_1 HTTP/1.1',
+                response: 304,
+                time: '17/May/2015:08:05:32 +0000',
+            },
+        },
+        {
+            _time: '2020-11-19T11:06:31.569479846Z',
+            _sysTime: '2020-11-19T11:06:31.581384524Z',
+            _rowId: 'c776x1uafnvq-4918f6cb9000095-1',
+            data: {
+                agent: 'Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)',
+                bytes: 0,
+                referrer: '-',
+                remote_ip: '93.180.71.3',
+                remote_user: '-',
+                request: 'GET /downloads/product_1 HTTP/1.1',
+                response: 304,
+                time: '17/May/2015:08:05:23 +0000',
+            },
+        },
+    ],
+    buckets: {
+        series: [],
+        totals: [],
+    },
+};
+
+const queryResult = {
+    request: {
+        startTime: '2020-11-19T11:06:31.569475746Z',
+        endTime: '2020-11-27T12:06:38.966791794Z',
+        resolution: 'auto',
+    },
+    ...queryLegacyResult,
+};
+
 describe('Client', () => {
     let client = new Client({ url: 'http://axiom-node-retries.dev.local' });
     expect(client).not.equal('undefined');
@@ -195,6 +254,23 @@ describe('Client', () => {
     });
 
     it('IngestString', async () => {
+        const scope = nock('http://axiom-node.dev.local');
+        const ingestStatus = {
+            ingested: 2,
+            failed: 0,
+            failures: [],
+            processedBytes: 630,
+            blocksCreated: 0,
+            walLength: 2,
+        };
+        scope.post('/api/v1/datasets/test/ingest').reply(function (_, body, cb) {
+            expect(this.req.headers).to.have.property('content-type');
+            expect(body).to.deep.equal([{ foo: 'bar' }, { foo: 'baz' }]);
+
+            cb(null, [200, ingestStatus]);
+        });
+
+
         const data = `[{"foo": "bar"}, {"foo": "baz"}]`;
         const response = await client.ingestString(
             'test',
@@ -208,6 +284,9 @@ describe('Client', () => {
     });
 
     it('Query', async () => {
+        const scope = nock('http://axiom-node.dev.local');
+        scope.post('/api/v1/datasets/test/query').reply(200, queryLegacyResult);
+        scope.post('/api/v1/datasets/test/query?streaming-duration=1m&nocache=true').reply(200, queryLegacyResult);
         // works without options
         let query = {
             startTime: '2020-11-26T11:18:00Z',
@@ -234,6 +313,9 @@ describe('Client', () => {
     });
 
     it('APL Query', async () => {
+        const scope = nock('http://axiom-node.dev.local');
+        scope.post('/api/v1/datasets/_apl?format=legacy').reply(200, queryResult);
+        scope.post('/api/v1/datasets/_apl?streaming-duration=1m&nocache=true&format=legacy').reply(200, queryResult);
         // works without options
         let response = await client.query("['test'] | where response == 304");
         expect(response).not.equal('undefined');
--- tests/unit/datasets.test.ts
+++ tests/unit/datasets.test.ts
@@ -24,74 +24,6 @@ describe('DatasetsService', () => {
             },
         ];
 
-        const ingestStatus = {
-            ingested: 2,
-            failed: 0,
-            failures: [],
-            processedBytes: 630,
-            blocksCreated: 0,
-            walLength: 2,
-        };
-
-        const queryLegacyResult = {
-            status: {
-                elapsedTime: 542114,
-                blocksExamined: 4,
-                rowsExamined: 142655,
-                rowsMatched: 142655,
-                numGroups: 0,
-                isPartial: false,
-                cacheStatus: 1,
-                minBlockTime: '2020-11-19T11:06:31.569475746Z',
-                maxBlockTime: '2020-11-27T12:06:38.966791794Z',
-            },
-            matches: [
-                {
-                    _time: '2020-11-19T11:06:31.569475746Z',
-                    _sysTime: '2020-11-19T11:06:31.581384524Z',
-                    _rowId: 'c776x1uafkpu-4918f6cb9000095-0',
-                    data: {
-                        agent: 'Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)',
-                        bytes: 0,
-                        referrer: '-',
-                        remote_ip: '93.180.71.3',
-                        remote_user: '-',
-                        request: 'GET /downloads/product_1 HTTP/1.1',
-                        response: 304,
-                        time: '17/May/2015:08:05:32 +0000',
-                    },
-                },
-                {
-                    _time: '2020-11-19T11:06:31.569479846Z',
-                    _sysTime: '2020-11-19T11:06:31.581384524Z',
-                    _rowId: 'c776x1uafnvq-4918f6cb9000095-1',
-                    data: {
-                        agent: 'Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)',
-                        bytes: 0,
-                        referrer: '-',
-                        remote_ip: '93.180.71.3',
-                        remote_user: '-',
-                        request: 'GET /downloads/product_1 HTTP/1.1',
-                        response: 304,
-                        time: '17/May/2015:08:05:23 +0000',
-                    },
-                },
-            ],
-            buckets: {
-                series: [],
-                totals: [],
-            },
-        };
-
-        const queryResult = {
-            request: {
-                startTime: '2020-11-19T11:06:31.569475746Z',
-                endTime: '2020-11-27T12:06:38.966791794Z',
-                resolution: 'auto',
-            },
-            ...queryLegacyResult,
-        };
-
         const scope = nock('http://axiom-node.dev.local');
 
         scope.get('/api/v1/datasets').reply(200, datasets);
@@ -100,16 +32,6 @@ describe('DatasetsService', () => {
         scope.put('/api/v1/datasets/test1').reply(200, datasets[1]);
         scope.delete('/api/v1/datasets/test1').reply(204);
         scope.post('/api/v1/datasets/test1/trim').reply(200, {});
-        scope.post('/api/v1/datasets/test/ingest').reply(function (_, body, cb) {
-            expect(this.req.headers).to.have.property('content-type');
-            expect(body).to.deep.equal([{ foo: 'bar' }, { foo: 'baz' }]);
-
-            cb(null, [200, ingestStatus]);
-        });
-        scope.post('/api/v1/datasets/test/query').reply(200, queryLegacyResult);
-        scope.post('/api/v1/datasets/test/query?streaming-duration=1m&nocache=true').reply(200, queryLegacyResult);
-        scope.post('/api/v1/datasets/_apl?format=legacy').reply(200, queryResult);
-        scope.post('/api/v1/datasets/_apl?streaming-duration=1m&nocache=true&format=legacy').reply(200, queryResult);
     });
 
     it('List', async () => {
