f3c69a1e00cd7a6abcf48b4b23a51a9e4e82914b
Islam Shehata <islam@axiom.co>
2023-02-24 12:14:21 +0200

    revert back to es5

--- packages/axiom-js/jest.config.ts
+++ packages/axiom-js/jest.config.ts
@@ -5,7 +5,8 @@ import { defaults as tsjPreset } from 'ts-jest/presets'
 const jestConfig: JestConfigWithTsJest = {
   transform: {
     ...tsjPreset.transform,
-  }
+  },
+  testEnvironment: 'node'
 };
 export default jestConfig;
 
--- packages/winston-axiom/jest.config.ts
+++ packages/winston-axiom/jest.config.ts
@@ -5,6 +5,7 @@ import { defaults as tsjPreset } from 'ts-jest/presets'
 const jestConfig: JestConfigWithTsJest = {
   transform: {
     ...tsjPreset.transform,
-  }
+  },
+  testEnvironment: 'node',
 };
 export default jestConfig;
