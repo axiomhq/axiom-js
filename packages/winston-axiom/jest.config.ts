99f169cd8b4a69527cbbac117fc898eaa349ca39
Islam Shehata <islam@axiom.co>
2023-02-24 13:14:18 +0200

    build for esnext by default

--- packages/winston-axiom/jest.config.ts
+++ packages/winston-axiom/jest.config.ts
@@ -1,11 +1,33 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
-import { defaults as tsjPreset } from 'ts-jest/presets'
+// const { pathsToModuleNameMapper } = require('ts-jest/utils');
 
+// import type { JestConfigWithTsJest } from 'ts-jest';
+// import { defaults as tsjPreset } from 'ts-jest/presets';
 
-const jestConfig: JestConfigWithTsJest = {
-  transform: {
-    ...tsjPreset.transform,
-  },
-  testEnvironment: 'node',
+// const jestConfig: JestConfigWithTsJest = {
+//   extensionsToTreatAsEsm: ['.ts'],
+//   moduleNameMapper: {
+//     '^(\\.{1,2}/.*)\\.js$': '$1',
+//   },
+//   transform: {
+//     '^.+\\.tsx?$': [
+//       'ts-jest',
+//       {
+//         useESM: true,
+//       },
+//     ],
+//     // ...tsjPreset.transform,
+//   },
+//   testEnvironment: 'node',
+// };
+// export default jestConfig;
+
+const jestConfig = {
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+      '^.+\\.ts?$': 'ts-jest',
+    },
+    testEnvironment: 'node',
 };
 export default jestConfig;
