071bad5412585be16d566ec84d3232cfc9d9c054
Islam Shehata <islam@axiom.co>
2023-03-20 11:56:33 +0200

    removed ununsed mock args

--- packages/axiom-js/test/lib/mock.ts
+++ packages/axiom-js/test/lib/mock.ts
@@ -1,31 +1,25 @@
 import { jest } from '@jest/globals';
 
 export const mockFetchResponse = (body: any, statusCode: number = 200, headers = {}) => {
-    const resp = new Response(JSON.stringify(body), { status: statusCode, headers })
-    const func: () => Promise<Response> = () => {
-        return Promise.resolve(resp);
-    }
+  const resp = new Response(JSON.stringify(body), { status: statusCode, headers })
+  const func: () => Promise<Response> = () => {
+    return Promise.resolve(resp);
+  }
 
-    jest.spyOn(global, 'fetch').mockImplementationOnce(func);
+  jest.spyOn(global, 'fetch').mockImplementationOnce(func);
 };
 
-interface mockArgs {
-    body: any;
-    status: number;
-    headers: HeadersInit;
-}
-
 export const testMockedFetchCall = (test: any, body: any, statusCode: number = 200, headers = {}) => {
-    const func: typeof fetch = (url:  RequestInfo | URL, init?: RequestInit): Promise<Response> => {
-        test(url, init);
-        return Promise.resolve(new Response(JSON.stringify(body), { status: statusCode, headers }));
-    }
+  const func: typeof fetch = (url: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
+    test(url, init);
+    return Promise.resolve(new Response(JSON.stringify(body), { status: statusCode, headers }));
+  }
 
-    jest.spyOn(global, 'fetch').mockImplementationOnce(func);
+  jest.spyOn(global, 'fetch').mockImplementationOnce(func);
 };
 
 export const mockNoContentResponse = () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => {
-        return Promise.resolve(new Response(null, { status: 204, statusText: 'No Content' }));
-    })
+  jest.spyOn(global, 'fetch').mockImplementationOnce(() => {
+    return Promise.resolve(new Response(null, { status: 204, statusText: 'No Content' }));
+  })
 };
