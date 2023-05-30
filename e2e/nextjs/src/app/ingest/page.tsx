e1d769ea9079ae7925c1a7c5643cd1b0cf49318b
Islam Shehata <islam@axiom.co>
2023-05-30 11:58:04 +0300

    use env vars without NEXT_PUBLIC

--- e2e/nextjs/src/app/ingest/page.tsx
+++ e2e/nextjs/src/app/ingest/page.tsx
@@ -7,9 +7,9 @@ export const fetchCache = 'force-no-store';
 export default async function IngestPage() {
   try {
     const client = new Client({
-      token: process.env.NEXT_PUBLIC_AXIOM_TOKEN,
-      orgId: process.env.NEXT_PUBLIC_AXIOM_ORG_ID,
-      url: process.env.NEXT_PUBLIC_AXIOM_URL,
+      token: process.env.AXIOM_TOKEN,
+      orgId: process.env.AXIOM_ORG_ID,
+      url: process.env.AXIOM_URL,
     });
 
     const resp = await client.ingest(
@@ -21,15 +21,11 @@ export default async function IngestPage() {
 
     return (
       <div>
-        <p>{process.env.AXIOM_TOKEN}, {process.env.AXIOM_ORG_ID}, {process.env.AXIOM_URL}</p>
-        <p>debug values: {process.env.NEXT_PUBLIC_AXIOM_TOKEN}, {process.env.NEXT_PUBLIC_AXIOM_ORG_ID}, {process.env.NEXT_PUBLIC_AXIOM_URL}</p>
         <p>ingested: {resp.ingested}, failed: {resp.failed}</p>
       </div>
     );
   } catch (err: any) {
     return <div>
-      <p>{process.env.AXIOM_TOKEN}, {process.env.AXIOM_ORG_ID}, {process.env.AXIOM_URL}</p>
-        <p>debug values: {process.env.NEXT_PUBLIC_AXIOM_TOKEN}, {process.env.NEXT_PUBLIC_AXIOM_ORG_ID}, {process.env.NEXT_PUBLIC_AXIOM_URL}</p>
       <p>failed to ingest, check console for errors</p>
     </div>;
   }
