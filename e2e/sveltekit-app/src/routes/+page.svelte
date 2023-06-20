6d783c74c4df8f46ab0591c201c1e5e12863f1e5
Islam Shehata <islam@axiom.co>
2023-06-20 16:05:07 +0300

    init svelte apps

---
+++ e2e/sveltekit-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Axiom } from '@axiomhq/js'
+
+    const axiom = new Axiom()
+    axiom.ingest('test', { message: 'Hello World!' })
+
+    axiom.flush()
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ packages/svelte/README.md
@@ -0,0 +1,15 @@
+# Axiom Svelte
+
+This is a [Svelte](https://svelte.dev/) plugin for [Axiom](https://axiom.co)
+
+## Installation
+
+```bash
+pnpm add @axiomhq/svelte
+```
+
+## Usage
+
+```js
+import { Logger } from "@axiomhq/svelte";
+```
