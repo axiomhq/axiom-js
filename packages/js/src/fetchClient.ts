f3c5723cc46ea4d24dd37afc03646526585d2f06
thecraftman <temitopetola@gmail.com>
2024-02-06 16:54:01 +0100

    fix ci build

--- packages/js/src/fetchClient.ts
+++ packages/js/src/fetchClient.ts
@@ -9,7 +9,7 @@ export class FetchClient {
     method: string,
     init: RequestInitWithRetry = {},
     searchParams: { [key: string]: string } = {}
-  ): Promise<T | null> {
+  ): Promise<{ data: T; status: number }> {
     let finalUrl = `${this.config.baseUrl}${endpoint}`;
     const params = this.prepareSearchParams(searchParams);
     if (params) {
@@ -20,27 +20,28 @@ export class FetchClient {
 
     const resp = await fetchRetry(fetch)(finalUrl, {
       retries: 3,
-      retryDelay: (attempt, error, response) => Math.pow(2, attempt) * 1000, // 1000, 2000, 4000
-      retryOn: [503, 502, 504, 500], // Retry on these HTTP status codes
+      retryDelay: (attempt, error, response) => Math.pow(2, attempt) * 1000,
+      retryOn: [503, 502, 504, 500],
       headers,
       method,
-      body: init.body ? JSON.stringify(init.body) : undefined, // Ensure body is stringified
+      body: init.body ? JSON.stringify(init.body) : undefined,
       ...init,
     });
 
     return this._handleResponse<T>(resp);
   }
 
-  private async _handleResponse<T>(resp: Response): Promise<T | null> {
+  private async _handleResponse<T>(resp: Response): Promise<{ data: T; status: number }> {
     if (resp.ok) {
-      if (resp.status === 204 || resp.headers.get('Content-Length') === '0') {
-        return null; 
+      let data: T = null as any;
+      if (!(resp.status === 204 || resp.headers.get('Content-Length') === '0')) {
+        data = await resp.json();
       }
-      return await resp.json() as T; 
+      return { data, status: resp.status };
     } else {
       if (resp.status === 429) {
-        const limit = parseLimitFromResponse(resp); 
-        throw new AxiomTooManyRequestsError(limit); 
+        const limit = parseLimitFromResponse(resp);
+        throw new AxiomTooManyRequestsError(limit);
       } else {
         if (resp.headers.get('Content-Length') !== '0') {
           const errorBody = await resp.json();
@@ -54,29 +55,24 @@ export class FetchClient {
   }
 
   private prepareSearchParams(searchParams: { [key: string]: string }): string {
-    for (const key in searchParams) {
-      if (!searchParams[key]) {
-        delete searchParams[key];
-      }
-    }
-
-    const params = new URLSearchParams(searchParams);
+    const cleanedParams = Object.fromEntries(Object.entries(searchParams).filter(([, value]) => value));
+    const params = new URLSearchParams(cleanedParams);
     return params.toString();
   }
 
-  post<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<T | null> {
+  post<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<{ data: T; status: number }> {
     return this.doReq<T>(url, 'POST', init, searchParams);
   }
 
-  get<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<T | null> {
+  get<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<{ data: T; status: number }> {
     return this.doReq<T>(url, 'GET', init, searchParams);
   }
 
-  put<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<T | null> {
+  put<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<{ data: T; status: number }> {
     return this.doReq<T>(url, 'PUT', init, searchParams);
   }
 
-  delete<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<T | null> {
+  delete<T>(url: string, init: RequestInitWithRetry = {}, searchParams: any = {}): Promise<{ data: T; status: number }> {
     return this.doReq<T>(url, 'DELETE', init, searchParams);
   }
 }
@@ -84,7 +80,7 @@ export class FetchClient {
 // Custom error class for handling 429 Too Many Requests error.
 export class AxiomTooManyRequestsError extends Error {
   constructor(public limit: Limit, public shortcircuit = false) {
-    super();
+    super(`Error: ${limit.type} limit exceeded. Try again later.`);
     Object.setPrototypeOf(this, AxiomTooManyRequestsError.prototype); // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
     const retryIn = AxiomTooManyRequestsError.timeUntilReset(limit);
     this.message = `${limit.type} limit exceeded, try again in ${retryIn.minutes}m${retryIn.seconds}s`;
@@ -94,7 +90,9 @@ export class AxiomTooManyRequestsError extends Error {
   }
 
   static timeUntilReset(limit: Limit) {
-    const total = limit.reset.getTime() - new Date().getTime();
+    const now = new Date().getTime();
+    const resetTime = limit.reset.getTime();
+    const total = resetTime - now;
     return {
       minutes: Math.floor(total / 60000),
       seconds: Math.floor((total % 60000) / 1000),
