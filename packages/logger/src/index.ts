387f9ed75c643f40c01d95c1b3f6d0fb981bbbc9
Islam Shehata <islam@axiom.co>
2024-02-26 11:55:49 +0200

    update the getDefaultLogger to respect AXIOM_LOG_LEVEL

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,4 @@
-import { LogLevel } from './levels';
+import { LOG_LEVEL, LogLevel } from './levels';
 import { Logger } from './logger';
 import { ConsoleTransport } from './transports/console';
 
@@ -7,8 +7,10 @@ export { LogEvent } from './event'
 export { LogLevel } from './levels'
 
 export function getDefaultLogger(): Logger {
+    const level = LogLevel[LOG_LEVEL as keyof typeof LogLevel];
+    
     return new Logger({
-        logLevel: LogLevel.debug,
+        logLevel: level,
         autoFlush: true,
         transformers: [],
         transport: new ConsoleTransport(),
--- packages/logger/test/unit/logLevels.spec.ts
+++ packages/logger/test/unit/logLevels.spec.ts
@@ -1,6 +1,6 @@
-import { test, expect, vi } from 'vitest';
-import { getDefaultLogger, Logger, LogLevel } from '../src/index.ts';
-import { ConsoleTransport } from '../src/transports/console.ts';
+import { describe, it, expect, vi } from 'vitest';
+import { getDefaultLogger, Logger, LogLevel } from '../../src/index';
+import { ConsoleTransport } from '../../src/transports/console';
 
 vi.hoisted(() => {
   // stub axiom env vars before importing logger
@@ -10,48 +10,56 @@ vi.hoisted(() => {
 
 vi.useFakeTimers();
 
-test('log levels', async () => {
-  const mockedConsole = vi.spyOn(console, 'log');
 
-  const log = getDefaultLogger();
+describe('Testing log levels', () => {
+  it('detects environment variable log level', async () => {
+    const mockedConsole = vi.spyOn(console, 'log');
+    const log = getDefaultLogger();
 
-  log.info('test');
-  await log.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(0);
+    log.config.logLevel = LogLevel.error;
 
-  // test overriding log level per logger
-  let logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.error, transport: new ConsoleTransport(), transformers: [] });
-  logger.debug('hello');
-  logger.info('hello');
-  logger.warn('hello');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(0);
+    log.info('test');
+    await log.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(0);
+  })
 
-  logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.warn, transport: new ConsoleTransport(), transformers: [] });
-  logger.info('hello');
-  logger.debug('hello');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(0);
-
-  logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.info, transport: new ConsoleTransport(), transformers: [] });
-  logger.debug('hello');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(0);
-
-  // disabled logging
-  logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.off, transport: new ConsoleTransport(), transformers: [] });
-  logger.error('no logs');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(0);
-
-  logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.error, transport: new ConsoleTransport(), transformers: [] });
-  logger.warn('warn');
-  logger.error('error');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(1);
-
-  logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.debug, transport: new ConsoleTransport(), transformers: [] });
-  logger.warn('hello');
-  await logger.flush();
-  expect(mockedConsole).toHaveBeenCalledTimes(2);
-});
+  it('log levels', async () => {
+    const mockedConsole = vi.spyOn(console, 'log');
+  
+    // test overriding log level per logger
+    let logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.error, transport: new ConsoleTransport(), transformers: [] });
+    logger.debug('hello');
+    logger.info('hello');
+    logger.warn('hello');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(0);
+  
+    logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.warn, transport: new ConsoleTransport(), transformers: [] });
+    logger.info('hello');
+    logger.debug('hello');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(0);
+  
+    logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.info, transport: new ConsoleTransport(), transformers: [] });
+    logger.debug('hello');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(0);
+  
+    // disabled logging
+    logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.off, transport: new ConsoleTransport(), transformers: [] });
+    logger.error('no logs');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(0);
+  
+    logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.error, transport: new ConsoleTransport(), transformers: [] });
+    logger.warn('warn');
+    logger.error('error');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(1);
+  
+    logger = new Logger({ args: {}, autoFlush: false, logLevel: LogLevel.debug, transport: new ConsoleTransport(), transformers: [] });
+    logger.warn('hello');
+    await logger.flush();
+    expect(mockedConsole).toHaveBeenCalledTimes(2);
+  });
+})
