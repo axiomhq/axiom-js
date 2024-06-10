16405b829ac00e243f668da33519e669c17e125d
Islam Shehata <islam@axiom.co>
2024-06-10 18:31:44 +0300

    Feat(annotations): Add annotations service

---
+++ packages/js/src/annotations.ts
@@ -0,0 +1,50 @@
+import HTTPClient from './httpClient.js';
+
+export namespace annotations {
+
+    export interface Annotation {
+        id: number;
+        type: string;
+        datasets: string[];
+        title?: string;
+        description?: string;
+        url?: string;
+        time?: string;
+        endTime?: string;
+    }
+
+    export interface ListingQueryParams {
+        datasets?: string[],
+        start?: string,
+        end?: string
+    }
+
+    export interface CreateRequest {
+        type: string;
+        datasets: string[];
+        title?: string;
+        description?: string;
+        url?: string;
+        time?: string;
+        endTime?: string;
+    }
+
+    export interface UpdateRequest {
+        description: string;
+    }
+
+    export class Service extends HTTPClient {
+        private readonly localPath = '/v2/annotations';
+
+        list = (req?: ListingQueryParams): Promise<Annotation[]> => this.client.get(this.localPath, {}, req);
+
+        get = (id: string): Promise<Annotation> => this.client.get(this.localPath + '/' + id);
+
+        create = (req: CreateRequest): Promise<Annotation> => this.client.post(this.localPath, { body: JSON.stringify(req) });
+
+        update = (id: string, req: UpdateRequest): Promise<Annotation> =>
+            this.client.put(this.localPath + '/' + id, { body: JSON.stringify(req) });
+
+        delete = (id: string): Promise<Response> => this.client.delete(this.localPath + '/' + id);
+    }
+}
