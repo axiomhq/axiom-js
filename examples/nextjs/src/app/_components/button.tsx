93ab4f6ebc97a3ee16f56eec12006cea807e469c
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-01-16 14:44:19 +0000

    docs: move handler to component body in example

--- examples/nextjs/src/app/_components/button.tsx
+++ examples/nextjs/src/app/_components/button.tsx
@@ -4,9 +4,14 @@ import { useLogger } from '@/lib/axiom/client';
 
 export default function Button() {
   const logger = useLogger();
+
+  const handleClick = () => {
+    logger.info('Hello World from Client Side!', { key: 'value' });
+  };
+
   return (
     <div>
-      <button style={{ color: 'red' }} onClick={() => logger.info('Hello World from Client Side!', { key: 'value' })}>
+      <button style={{ color: 'red' }} onClick={handleClick}>
         Log
       </button>
     </div>
