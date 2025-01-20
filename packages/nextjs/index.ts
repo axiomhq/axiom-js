0ef11fb2ab40bbdf5f0f51630ce0cb509ea9598e
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-01-20 14:45:54 +0000

    feat: Add onRequestError helpers

--- packages/nextjs/index.ts
+++ packages/nextjs/index.ts
@@ -1,3 +1,4 @@
 export * from './routeHandler';
 export * from './proxyRouteHandler';
 export * from './middleware';
+export * from './instrumentation';
---
+++ packages/nextjs/instrumentation.ts
@@ -0,0 +1,38 @@
+import { onRequestError } from '@/instrumentation';
+import { Logger } from '@axiomhq/logger';
+import { Instrumentation } from 'next';
+
+export const transformOnRequestError = (
+  ...args: Parameters<Instrumentation.onRequestError>
+): [message: string, report: Record<string, any>] => {
+  const [error, request, context] = args;
+  if (error instanceof Error) {
+    return [
+      error.message,
+      {
+        ...error,
+        error: error.name,
+        cause: error.cause,
+        stack: error.stack,
+        digest: (error as Error & { digest?: string }).digest,
+        request: request,
+        context: context,
+      },
+    ];
+  }
+  return [
+    `${request.method} ${request.path} ${context.routeType}`,
+    {
+      error,
+      request: request,
+      context: context,
+    },
+  ];
+};
+
+export const createOnRequestError =
+  (logger: Logger): Instrumentation.onRequestError =>
+  async (...args) => {
+    logger.error(...transformOnRequestError(...args));
+    await logger.flush();
+  };
