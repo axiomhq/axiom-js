2c35c11b970d593410d58f85370c3008f91b8ee5
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2025-02-04 15:25:58 +0000

    chore: add globals to vitest

--- packages/nextjs/vitest.config.ts
+++ packages/nextjs/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig(() => ({
   test: {
+    environment: 'node',
+    globals: true,
     setupFiles: ['./test/lib/setupNext.ts'],
     env: {
       __NEXT_EXPERIMENTAL_AUTH_INTERRUPTS: 'true', // Allows for forbidden() and unauthorized() to work
