a3afcb5c17f4e82d862e93cda4533398b3f2a42a
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-05-20 15:09:40 +0100

    feat: remove any reference to the node:crypto lib

--- packages/nextjs/src/lib/node-utils.ts
+++ packages/nextjs/src/lib/node-utils.ts
@@ -1,4 +1,27 @@
-const crypto = globalThis.crypto ?? require('node:crypto').webcrypto;
+const crypto = {
+  /**
+   * Try to use the native **crypto** library through globalThis.crypto,
+   * if it's not present, use a less cryptographically secure internal function.
+   *
+   * This is meant to remove any reference to **node:crypto**, as it causes Vercel deployments to fail
+   */
+  randomUUID: () => {
+    if (globalThis.crypto) {
+      return globalThis.crypto.randomUUID();
+    }
+    return generateUUIDv4();
+  },
+};
+
+function generateUUIDv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
+    const rand = Math.random() * 16 | 0;
+    const value = char === 'x' ? rand : (rand & 0x3) | 0x8;
+    return value.toString(16);
+  });
+}
+
+
 const AsyncLocalStorage = globalThis.AsyncLocalStorage ?? require('node:async_hooks').AsyncLocalStorage;
 
 export { crypto, AsyncLocalStorage };
